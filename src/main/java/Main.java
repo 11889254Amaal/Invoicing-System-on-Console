cec312d348f90708c8b6f8ee29a3914d905399a2
AmaalAljasria <a26j123@gmail.com>
2023-01-29 10:48:01 +0400

    add menu class with all class and function

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+	static void ApplicationMainMenuChoicesFunction() {
+
+		System.out.println("******************************************************");
+		System.out.println("   Invoicing system for a groceries shop Main Menu    ");
+		System.out.println("******************************************************");
+		System.out.println("Welcome...Please Select one of the following options:");
+		System.out.println("1. connect to database");
+		System.out.println("2. Read from Api");
+		System.out.println("3. Read from API by Books class");
+		System.out.println("4. Read from API by Auther class");
+		System.out.println("5. Read from API by Artical class");
+		System.out.println("6. Creat books table");
+		System.out.println("7. Insert to Table books");
+		System.out.println("8. create Authors Table");
+		System.out.println("9. Insert to Authors Table");
+		System.out.println("10. Create Table Articals");
+		System.out.println("11. Insert to Table Artical");
+		System.out.println("******************************************************");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+	}
+
+}
